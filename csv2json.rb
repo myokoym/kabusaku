d2d362f01cff21beda32aac1bbba40953cccc7d8
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-03-04 21:39:58 +0900

    To add date to database

--- csv2json.rb
+++ csv2json.rb
@@ -5,13 +5,15 @@ require "json"
 
 csv_path = ARGV[0]
 
+date = csv_path.scan(/\d{4}-\d{2}-\d{2}/).first
+
 rows = []
 CSV.foreach(csv_path, encoding: "Shift_JIS:UTF-8") do |row|
-  rows << row
+  rows << [date, *row]
 end
 
 rows.shift
 rows.shift
-rows.unshift(%w(_key market name business start high low finish volume trading_value))
+rows.unshift(%w(date code market name business start high low finish volume trading_value))
 
 JSON.dump(rows, $stdout)
